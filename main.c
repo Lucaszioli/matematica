d8a9737f25b1aab6f9857e5a66dec23ff369198c
Lucas Zioli <lucas.zioli.04@gmail.com>
2024-03-12 19:22:17 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "matematica.h"
+#include <stdio.h>
+
+void main(){
+	float angulo_radianos;
+	printf("Digite o angulo em radianos: ");
+	scanf("%f", &angulo_radianos);
+	printf("Seno: %f\n", calcular_seno(angulo_radianos));
+	printf("Cosseno: %f\n", calcular_cosseno(angulo_radianos));
+	printf("Tangente: %f\n", calcular_tangente(angulo_radianos));
+}
---
+++ matematica.h
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @param angulo_radianos
+ * 
+ * 
+ * @return
+ * 
+*/ float calcular_seno(float angulo_radianos);
+
+/**
+ * 
+ * @param angulo_radianos
+ * 
+ * 
+ * @return
+ * 
+*/ float calcular_cosseno(float angulo_radianos);
+
+/**
+ * 
+ * @param angulo_radianos
+ * 
+ * 
+ * @return
+ * 
+*/ float calcular_tangente(float angulo_radianos);
+
+
