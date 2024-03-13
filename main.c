1613f13b1432ab7e6de25e54cafb8d52e0e3c6d6
Lucas Zioli <lucas.zioli.04@gmail.com>
2024-03-12 21:35:58 -0300

    Adiciona comentarios nos .c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 void main(){
-	float angulo_radianos;
-	printf("Digite o angulo em radianos: ");
-	scanf("%f", &angulo_radianos);
+	float angulo_radianos; //Criando variavel para armazenar o angulo em radianos
+	printf("Digite o angulo em radianos: "); //Pedindo para o usuario digitar o angulo em radianos
+	scanf("%f", &angulo_radianos); //Lendo o angulo em radianos
 	printf("Seno: %f\n", calcular_seno(angulo_radianos));
 	printf("Cosseno: %f\n", calcular_cosseno(angulo_radianos));
 	printf("Tangente: %f\n", calcular_tangente(angulo_radianos));
--- matematica.c
+++ matematica.c
@@ -1,15 +1,15 @@
 #include "matematica.h"
 #include <math.h>
 #include <stdio.h>
-
+//Calcula o seno de um angulo em radianos
 float calcular_seno(float angulo_radianos){
 	return sin(angulo_radianos);
 }
-
+//Calcula o cosseno de um angulo em radianos
 float calcular_cosseno(float angulo_radianos){
 	return cos(angulo_radianos);
 }
-
+//Calcula a tangente de um angulo em radianos
 float calcular_tangente(float angulo_radianos){
 	return tan(angulo_radianos);
 }
