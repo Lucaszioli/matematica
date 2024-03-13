f23d5fe68887b1ca88477bfe0e9003251ee0ab82
Lucas Zioli <lucas.zioli.04@gmail.com>
2024-03-12 21:24:49 -0300

    Adiciona comentarios no header

--- matematica.h
+++ matematica.h
@@ -1,27 +1,27 @@
-/**
+/** Calcula o seno do angulo em radianos
  * 
- * @param angulo_radianos
+ * @param angulo_radianos Angulo em radianos
  * 
  * 
- * @return
+ * @return O seno do angulo
  * 
 */ float calcular_seno(float angulo_radianos);
 
-/**
+/** Calcula o cosseno do angulo em radianos
  * 
- * @param angulo_radianos
+ * @param angulo_radianos Angulo em radianos
  * 
  * 
- * @return
+ * @return O cosseno do angulo
  * 
 */ float calcular_cosseno(float angulo_radianos);
 
-/**
+/** Calcula a tangente do angulo em radianos
  * 
- * @param angulo_radianos
+ * @param angulo_radianos Angulo em radianos
  * 
  * 
- * @return
+ * @return A tangente do angulo
  * 
 */ float calcular_tangente(float angulo_radianos);
 
